1d817cb54fe92c6e3c93c6bed3dd9563e0088571
madrid-7 <783798088@qq.com>
2021-02-20 21:51:19 +0800

    MavenExercise，Mysql

---
+++ MavenTest/src/main/java/com/zxf/boke.java
@@ -0,0 +1,14 @@
+package com.zxf;
+
+/**
+ * @author ：ZXF
+ * @program: MavenTest
+ * @description: bokexitong
+ * @date ：2021-02-17 09:04
+ */
+
+public class boke {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ MavenTest/博客系统的建库建表语句.sql
@@ -0,0 +1,38 @@
+-- 建库
+CREATE DATABASE boke CHARSET utf8mb4;
+use boke;
+
+-- 建表
+-- 用户表
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT '自增 id',
+    username VARCHAR(200) NOT NULL UNIQUE COMMENT '唯一的用户名',
+    nikename VARCHAR(200) NOT NULL COMMENT '显示名称',
+    password VARCHAR(200) NOT NULL COMMENT '登录密码'
+);
+
+-- 文章表
+CREATE TABLE articles (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT '自增 id',
+    author_id INT NOT NULL COMMENT '作者 id',
+    title VARCHAR(200) NOT NULL COMMENT '文章标题',
+    published_at DATETIME NOT NULL COMMENT '文章发表时间',
+    content TEXT NOT NULL COMMENT '文章正文'
+);
+
+-- 评论表
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT '自增 id',
+    user_id INT NOT NULL COMMENT '评论者id',
+    article_id INT NOT NULL COMMENT '文章id',
+    published_at DATETIME NOT NULL COMMENT '评论发表时间',
+    content VARCHAR(200) NOT NULL COMMENT '评论正文'
+);
+
+-- 点赞关系表
+-- 使用复合主键的形式
+CREATE TABLE like_relations (
+    user_id INT NOT NULL COMMENT '评论者 id',
+    article_id INT NOT NULL COMMENT '文章 id',
+    PRIMARY KEY (user_id, article_id)
+);
