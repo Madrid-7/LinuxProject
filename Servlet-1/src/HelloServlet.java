e741f3f7fbb2ab2a62100adedd9f04ababf8d7d8
madrid-7 <783798088@qq.com>
2021-03-17 23:03:55 +0800

    WebTest

---
+++ Servlet-1/src/HelloServlet.java
@@ -0,0 +1,23 @@
+/**
+ * @author ：ZXF
+ * @program: Servlet-1
+ * @description: test
+ * @date ：2021-03-17 19:25
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            IOException, ServletException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>这是动态的内容</h1>");
+    }
+}
